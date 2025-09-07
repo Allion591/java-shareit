6cd92e3563abd1e4a69985d37e866c98e33d177f
Allion591 <leo59158@yandex.ru>
2025-09-07 12:25:33 +0500

    Реализовал хранение и присвоение id, в предидущей версии не реализовывал так как не требовалось, была заготовка

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepositoryImpl.java
@@ -1,17 +1,27 @@
 package ru.practicum.shareit.booking.repository;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.exceptions.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
 
+@Slf4j
 @Repository
 public class BookingRepositoryImpl implements BookingRepository {
-    Booking booking = new Booking();
+    private static final Set<Booking> bookings = new HashSet<>();
+    private final AtomicLong generateIds = new AtomicLong(0);
 
     @Override
     public Booking save(Booking booking) {
+        booking.setId(generateIds.incrementAndGet());
+        log.info("Сохраняю бронь : {}", booking.getId());
+        bookings.add(booking);
         return booking;
     }
 
@@ -22,7 +32,10 @@ public class BookingRepositoryImpl implements BookingRepository {
 
     @Override
     public Booking getById(Long bookingId) {
-        return booking;
+        return bookings.stream()
+                .filter(booking -> booking.getId().equals(bookingId))
+                .findFirst()
+                .orElseThrow(()-> new NotFoundException("Бронь не найдена")) ;
     }
 
     @Override
