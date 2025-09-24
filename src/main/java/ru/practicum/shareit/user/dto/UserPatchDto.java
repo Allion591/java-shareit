e60c9ef788c41d7dcf42149e5bddfb64001b75c4
Allion591 <leo59158@yandex.ru>
2025-09-24 13:15:34 +0500

    Хранение пользователей реализовано в БД

--- src/main/java/ru/practicum/shareit/user/dto/UserPatchDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserPatchDto.java
@@ -1,4 +1,16 @@
 package ru.practicum.shareit.user.dto;
 
-public class UserPathDto {
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Data
+public class UserPatchDto {
+    private Optional<Long> id = Optional.empty();
+    private Optional<String> name = Optional.empty();
+    @Email(message = "Неверный формат электронной почты")
+    private Optional<String> email = Optional.empty();
+    private Optional<String> login = Optional.empty();
+    private Optional<LocalDate> birthday = Optional.empty();
 }
