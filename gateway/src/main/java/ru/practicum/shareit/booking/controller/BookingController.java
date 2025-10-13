3ba97bc6e5c20ecd06e65646a8b739b9e474deb8
Allion591 <leo59158@yandex.ru>
2025-10-13 15:27:47 +0500

    Добавил валидацию всех полей

--- gateway/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -1,15 +1,21 @@
 package ru.practicum.shareit.booking.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.client.BookingClient;
 import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.BookingRequestParams;
+
 import java.util.List;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/bookings")
 @RequiredArgsConstructor
@@ -18,43 +24,39 @@ public class BookingController {
 
     @PostMapping
     public ResponseEntity<Object> createBooking(@RequestBody @Valid BookingDto bookingDto,
-                                                @RequestHeader("X-Sharer-User-Id") Long userId) {
+                                                @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
         log.info("Шлюз принял запрос на сохранение бронирования: {}, {}", bookingDto.getItemId(), userId);
         return bookingClient.create(bookingDto, userId);
     }
 
     @PatchMapping("/{bookingId}")
-    public ResponseEntity<Object> updateBookingStatus(@PathVariable Long bookingId,
-                                                      @RequestParam Boolean approved,
-                                                      @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ResponseEntity<Object> updateBookingStatus(@PathVariable @Min(1) Long bookingId,
+                                                      @RequestParam @NotNull Boolean approved,
+                                                      @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
         log.info("Шлюз принял запрос на обновление бронирования: {}, {}", bookingId, userId);
         return bookingClient.updateStatus(bookingId, userId, approved);
     }
 
     @GetMapping("/{bookingId}")
-    public ResponseEntity<Object> getBooking(@PathVariable Long bookingId,
-                                             @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public ResponseEntity<Object> getBooking(@PathVariable @Min(1) Long bookingId,
+                                             @RequestHeader("X-Sharer-User-Id") @Min(1) Long userId) {
         log.info("Шлюз принял запрос на вывод бронирования: {}, {}", bookingId, userId);
         return bookingClient.getById(userId, bookingId);
     }
 
     @GetMapping
-    public ResponseEntity<List<Object>> getUserBookings(
-            @RequestHeader("X-Sharer-User-Id") Long userId,
-            @RequestParam(defaultValue = "ALL") String state,
-            @RequestParam(defaultValue = "0") int from,
-            @RequestParam(defaultValue = "10") int size) {
+    public ResponseEntity<List<Object>> getUserBookings1(@RequestHeader("X-Sharer-User-Id")
+                                                             @Min(1) Long userId,
+            @ModelAttribute @Valid BookingRequestParams params) {
         log.info("Шлюз принял запрос на вывод всех броней пользователя: {}", userId);
-        return bookingClient.getUserBookings(userId, state, from, size);
+        return bookingClient.getUserBookings(userId, params.getState(), params.getFrom(), params.getSize());
     }
 
     @GetMapping("/owner")
-    public ResponseEntity<List<Object>> getOwnerBookings(
-            @RequestHeader("X-Sharer-User-Id") Long userId,
-            @RequestParam(defaultValue = "ALL") String state,
-            @RequestParam(defaultValue = "0") int from,
-            @RequestParam(defaultValue = "10") int size) {
+    public ResponseEntity<List<Object>> getOwnerBookings(@RequestHeader("X-Sharer-User-Id")
+                                                             @Min(1) Long userId,
+                                                         @ModelAttribute @Valid BookingRequestParams params) {
         log.info("Шлюз принял запрос на вывод всех броней владельца: {}", userId);
-        return bookingClient.getOwnerBookings(userId, state, from, size);
+        return bookingClient.getOwnerBookings(userId, params.getState(), params.getFrom(), params.getSize());
     }
 }
